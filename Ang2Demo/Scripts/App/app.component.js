47f0ba971f67c57beea493ab2aff4a3cb0ad0a39
Muhammad Hassan <hassantariq_8631@hotmail.com>
2016-10-18 14:58:03 -0500

    update content

--- Ang2Demo/Scripts/App/app.component.js
+++ Ang2Demo/Scripts/App/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App - Demo</h1>'
+            template: '<h1>Angular App - Sample Demo</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- Ang2Demo/Scripts/App/app.component.ts
+++ Ang2Demo/Scripts/App/app.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App - Demo</h1>'
+    template: '<h1>Angular App - Sample Demo</h1>'
 })
 export class AppComponent { }
